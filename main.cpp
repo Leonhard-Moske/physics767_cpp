86525e6e758b8762e633a78d19304f90da7dbb61
Leonhard-Moske <leonhard.moske@gmail.com>
2024-10-12 16:40:45 +0200

    final touches

--- main.cpp
+++ main.cpp
@@ -12,6 +12,73 @@
 
 #include <memory>
 
+/*
+class Function {
+public:
+    // Constructor that takes coefficients for a quadratic function ax^2 + bx + c
+    Function(double a, double b, double c) : coeffs(Eigen::Vector3d(a, b, c)) {}
+
+    // Evaluate the function at a given x
+    double evaluate(double x) const {
+        return coeffs(0) * x * x + coeffs(1) * x + coeffs(2);
+    }
+
+    // Evaluate the function at multiple points and store in a map
+    std::map<double, double> evaluateAtPoints(const std::vector<double>& points) {
+        std::map<double, double> results;
+        for (double x : points) {
+            results[x] = evaluate(x);
+        }
+        return results;
+    }
+
+    // Print the coefficients
+    void printCoefficients() const {
+        std::cout << "Coefficients: " << coeffs.transpose() << std::endl;
+    }
+
+private:
+    Eigen::Vector3d coeffs; // Coefficients of the function
+};
+*/
+
+/*
+class HubbardHamiltonian {
+public:
+    HubbardHamiltonian(int size, double t, double U)
+        : size(size), t(t), U(U), hamiltonian(Eigen::MatrixXd::Zero(size, size)) {
+        buildHamiltonian();
+    }
+
+    const Eigen::MatrixXd& getHamiltonian() const {
+        return hamiltonian;
+    }
+
+    void printHamiltonian() const {
+        std::cout << "Hubbard Hamiltonian Matrix:\n" << hamiltonian << std::endl;
+    }
+
+private:
+    int size;                 // Number of sites
+    double t;                // Hopping parameter
+    double U;                // On-site interaction strength
+    Eigen::MatrixXd hamiltonian;
+
+    void buildHamiltonian() {
+        for (int i = 0; i < size; ++i) {
+            // Hopping terms
+            if (i < size - 1) {
+                hamiltonian(i, i + 1) = -t; // Hop to the right
+                hamiltonian(i + 1, i) = -t; // Hop to the left
+            }
+            // On-site interaction
+            hamiltonian(i, i) += U; // Add on-site interaction
+        }
+    }
+};
+*/
+
+
 class Car
 {
 public:
@@ -63,7 +130,7 @@ double add(double a, double b)
     return a + b;
 }
 
-int main()
+int main(int argc, char** argv)
 {
     std::cout << "Hello World!" << std::endl;
 
@@ -110,18 +177,19 @@ int main()
         std::cout << vec[i] << std::endl;
     }
 
-    //
+    // vfr
 
     std::map<std::string, double> map;
 
     map["one"] = 1.0;
-    map["two"] = 2.0;
+    map["two"] = 2.2;
     map["three"] = 3.0;
 
     for (auto m : map)
     {
         std::cout << m.first << " : " << m.second << std::endl;
     }
+    std::cout << map["two"] << std::endl;
 
     //
 
---
+++ script.md
@@ -0,0 +1,41 @@
+# Script for CPP introduction
+
+## Requirements
+- VSCode
+- SSH extention
+- eduroam
+- github account or gitlab
+
+## Access to Cip Pool
+https://www.physik-astro.uni-bonn.de/de/fachgruppe/cip-pool-physik
+'''ssh ihre-uni-id@cip-pool.physik.uni-bonn.de'''
+
+https://code.visualstudio.com/docs/remote/ssh-tutorial
+
+## Git 
+- explain why git
+- explain the concept of a repo
+- git init
+- but better git clone
+- git add
+- git commit
+- (git push)
+
+## Hello world
+- make document
+- hello world
+    explain the io stuff
+- gcc
+
+## Cmake
+- make build dir
+- make cmakelist
+- show how to build
+
+DOES IT WORK WITH EVERYONE?
+
+## Go through the document
+
+## Let them write the polynomial
+
+## Let them write the hubbard hamiltonian
